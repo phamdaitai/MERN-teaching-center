de949d93fe9cfc1961456f36240e8459beb399ef
phamdaitai <phamdaitai9899@gmail.com>
2019-11-26 00:21:50 +0700

    slider home

--- client/src/pages/home/homeMiddle/index.js
+++ client/src/pages/home/homeMiddle/index.js
@@ -1,7 +1,36 @@
 import React, { Component } from 'react';
+import ItemsCarousel from 'react-items-carousel';
+import { Button, Icon } from 'antd';
 import './style.css';
 
+const noOfItems = 12;
+const noOfCards = 5;
+const autoPlayDelay = 2000;
+
 class HomeMiddle extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeItemIndex: 0
+    }
+  }
+
+  state = {
+    activeItemIndex: 0,
+  };
+
+  componentDidMount() {
+    this.interval = setInterval(this.tick, autoPlayDelay);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  tick = () => this.setState(prevState => ({
+    activeItemIndex: (prevState.activeItemIndex + 1) % (noOfItems - noOfCards + 1),
+  }));
+
   render() {
     return (
       <div className="home-middle">
@@ -10,7 +39,38 @@ class HomeMiddle extends Component {
         </div>
         <div className="home-middle-content">
           <div className="home-middle-content-inner">
-
+            <div className="home-middle-carouse">
+              <ItemsCarousel
+                gutter={12}
+                slidesToScroll={1}
+                numberOfCards={noOfCards}
+                activeItemIndex={this.state.activeItemIndex}
+                requestToChangeActive={this.onChange}
+                activeItemIndex={this.state.activeItemIndex}
+                requestToChangeActive={value => this.setState({ activeItemIndex: value })}
+                rightChevron={
+                  <Button shape="circle">
+                    <Icon type="right" />
+                  </Button>
+                }
+                leftChevron={
+                  <Button shape="circle">
+                    <Icon type="left" />
+                  </Button>
+                }
+              >
+                <div>a</div>
+                <div>b</div>
+                <div>c</div>
+                <div>d</div>
+                <div>e</div>
+                <div>f</div>
+                <div>c</div>
+                <div>d</div>
+                <div>e</div>
+                <div>f</div>
+              </ItemsCarousel>
+            </div>
           </div>
         </div>
       </div>
